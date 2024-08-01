65d2d2911075304069755c154c7690ee28266dd6
Imran751 <muhammadimranjhang@gmail.com>
2024-08-01 16:01:38 +0500

    Starting Portfolio

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={styles.container}>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    // justifyContent: "center",
+    // alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f5f5f5",
+  },
+};
+
+export default App;
