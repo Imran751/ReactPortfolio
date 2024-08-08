5d0463f528cdbdf29c5c35f8dc05285630ff200a
Imran751 <muhammadimranjhang@gmail.com>
2024-08-08 16:47:19 +0500

    Started Catogies

--- src/Components/3Profile/Profile.jsx
+++ src/Components/3Profile/Profile.jsx
@@ -1,9 +1,18 @@
+import React from "react";
 import "./Profile.css";
 
 export default function Profile() {
+  const categories = ["feature", "figma", "website", "assets", "store"];
+
   return (
     <div className="Profile" id="Profile">
-      <p>Profile</p>
+      <div className="category-container">
+        {categories.map((category, index) => (
+          <div key={index} className="category-item">
+            {category}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
