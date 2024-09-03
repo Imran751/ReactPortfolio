2456ebcdaff23ff26ab17c3181dd719e817f601b
Imran751 <muhammadimranjhang@gmail.com>
2024-09-03 08:34:11 +0500

    Footer Section done

--- src/Components/7Footer/Footer.jsx
+++ src/Components/7Footer/Footer.jsx
@@ -3,7 +3,49 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer" id="Footer">
-      <p>Footer</p>
+      <div className="SocialContainer">
+              <a
+                href="https://github.com/yourusername"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={require("../../Assets/git.png")} alt="GitHub" />
+              </a>
+              <a
+                href="https://youtube.com/yourchannel"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={require("../../Assets/youtube.png")} alt="YouTube" />
+              </a>
+              <a
+                href="https://twitter.com/yourusername"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={require("../../Assets/x.png")} alt="Twitter" />
+              </a>
+              <a
+                href="https://instagram.com/yourusername"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={require("../../Assets/insta.png")} alt="Instagram" />
+              </a>
+              <a
+                href="https://linkedin.com/in/yourusername"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={require("../../Assets/linkedin.png")}
+                  alt="LinkedIn"
+                />
+              </a>
+            </div>
+            <div className="allrights">
+              <p>All rights are reserved by <span>Muhammad Imran</span></p>
+            </div>
     </div>
   );
 }
